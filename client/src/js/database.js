d351d2429c967b397cf5855769e52284581e2bd4
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2023-02-06 23:07:26 -0600

    database.js with PUT & GET functions added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,43 @@ const initdb = async () =>
   });
 
 // TODO: Agregar lógica a un método que acepte algo de contenido y lo agregue a la base de datos
-export const putDb = async (content) => console.error('putDb not implemented');
+// export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  console.log('PUT to the database');
+  
+  const jateDb = await openDB('jate', 1);
+
+  const txn = jateDb.transaction('jate', 'readwrite');
+
+  const store = txn.objectStore('jate');
+
+  const request = store.put({ content: content });
+
+  const result = await request;
+  console.log('data saved to the database', result);
+
+}
 
 // TODO: Agregar lógica para un método que obtiene todo el contenido de la base de datos
-export const getDb = async () => console.error('getDb not implemented');
+//export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async (content) => {
+
+  console.log('GET from database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const txn = jateDb.transaction('jate', 'readonly');
+
+  const store = txn.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.content:', result);
+  return result;
+
+}
 
 initdb();
